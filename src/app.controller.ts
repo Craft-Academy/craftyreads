3ddc7b664b69186c7dddda0d7814eac35122c0fd
Pierre Criulanscy <pierrecriulanscy@gmail.com>
2024-05-31 18:25:25 +0200

    test: add naive form implementation to make the test pass

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Query } from '@nestjs/common';
+import { AddBookUseCase } from './add-book.usecase';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly addBookUseCase: AddBookUseCase) {}
 
   @Get()
-  getHello(): string {
+  getHello(@Query() query?: { title: string }): string {
     return `
 <!DOCTYPE html>
 <html>
@@ -15,6 +15,7 @@ export class AppController {
   </head>
   <body>
     <form>
+    ${query?.title ? '<p>Book added</p>' : ''}
       <label for="title">Title</label>
       <input type="text" id="title" name="title">
       <button type="submit">Add book</button>
